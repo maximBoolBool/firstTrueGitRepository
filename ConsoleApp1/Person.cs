63601a6a6b6551a3045e74e5933380d5e3c87241
Plebey <remezmax117@gmail.com>
2023-02-20 23:40:12 +0300

    created new

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -4,4 +4,12 @@ public class Person
 {
     public DateTime BirthdayDate { get; set; }
     public string Name { get; set; }
+
+    public string ToString
+    {
+        get
+        {
+            return BirthdayDate.ToString() + " " + Name;
+        }
+    }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using ConsoleApp1;
+
+var person = new Person();
+person.Name = "Max";
+person.BirthdayDate = DateTime.Now;
+Console.WriteLine(person.ToString);
